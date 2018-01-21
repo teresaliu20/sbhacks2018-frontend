28c8dd8252fe4996db4c7d269af803909a919fac
teresaliu20 <teresali@usc.edu>
2018-01-21 08:57:34 -0800

    added police reports

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import MapPage from "./pages/MapPage";
 import HomePage from "./pages/HomePage";
+import AdminMapPage from "./pages/AdminMapPage";
 import "./App.css";
 
 const App = () => (
-	<HashRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/map" component={MapPage} />
+      <Route exact path="/admin" component={AdminMapPage} />
       <Route render={() => "404 Page"} />
     </Switch>
-    </HashRouter>
+  </HashRouter>
 );
 
-export default (App);
+export default App;
---
+++ src/components/AdminHeader/index.jsx
@@ -0,0 +1,19 @@
+import _ from "lodash";
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.css";
+
+const enhance = _.identity;
+
+const AdminHeader = ({ watchers, forks, repositories }) => (
+  <div id="admin-header" className="header">
+    <h1 id="title">
+      Alpha <span className="italic">ADMIN</span>
+    </h1>
+    <Link to={`/`} id="home-button">
+      Home
+    </Link>
+  </div>
+);
+
+export default enhance(AdminHeader);
