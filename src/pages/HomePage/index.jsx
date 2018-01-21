859376a522443329f409b438ffc998c508d68048
teresaliu20 <teresali@usc.edu>
2018-01-21 09:12:56 -0800

    added fav

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,6 +3,7 @@ import styles from "./styles.css";
 import firebase from "firebase";
 import firebaseui from "firebaseui";
 import firebasestyles from "./firebaseui.css";
+import logo from "./alfie.png";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { firebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
@@ -11,8 +12,9 @@ class HomePage extends Component {
   render() {
     return (
       <div className="home-page">
+        <img id="logo" src={logo} />
         <h1 id="title">Alpha</h1>
-        <h2>Security through numbers</h2>
+        <h2 id="phrase">Security through numbers</h2>
         <div id="firebaseui-auth-container" />
       </div>
     );
